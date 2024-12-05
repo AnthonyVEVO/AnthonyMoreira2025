80d5daff0384956adaa971d5aefb3623c38fd773
Anthony Moreira <master098635@gmail.com>
2024-12-05 14:37:47 -0500

    Poductos

--- src/main/java/ec/edu/espoch/anthonymoreira2025/clases/Productos.java
+++ src/main/java/ec/edu/espoch/anthonymoreira2025/clases/Productos.java
@@ -9,5 +9,20 @@ package ec.edu.espoch.anthonymoreira2025.clases;
  * @author SO-LAB1-PC28
  */
 public class Productos {
+    public String nombre;
+    public Double precio;
+    public String categoria;
+    
+    public Productos(String nombre, Double precio, String categoria){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.categoria=categoria;
+        
+        public tipoProducto();
+    public double calcularPrecioTotal();
+    return precioTotal
+    }
+    
+    
     
 }
