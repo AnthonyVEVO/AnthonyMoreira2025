1e667e17f15dbd25ceb1d94de80d61fc6d74312b
Anthony Moreira <master098635@gmail.com>
2024-12-05 14:02:08 -0500

    Creacion de repositorio

---
+++ src/main/java/ec/edu/espoch/anthonymoreira2025/AnthonyMoreira2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.anthonymoreira2025;
+
+/**
+ *
+ * @author SO-LAB1-PC28
+ */
+public class AnthonyMoreira2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ec/edu/espoch/anthonymoreira2025/enumeraciones/Categoria.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.anthonymoreira2025.enumeraciones;
+
+/**
+ *
+ * @author SO-LAB1-PC28
+ */
+public class Categoria {
+    
+}
